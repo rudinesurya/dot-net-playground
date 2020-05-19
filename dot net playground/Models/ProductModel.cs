6863c367dc43312eb9d3561682e13f0ca35ab774
rudinesurya <rudinesurya@gmail.com>
2020-05-19 14:21:30 +0100

    Add project files.

---
+++ dot net playground/Models/ProductModel.cs
@@ -0,0 +1,58 @@
+﻿using dot_net_playground.Models.Base;
+
+namespace dot_net_playground.Models
+{
+    public class ProductModel : ObservableObject
+    {
+        #region Fields
+
+        private int _productId;
+        private string _productName;
+        private decimal _unitPrice;
+
+        #endregion // Fields
+
+        #region Properties
+
+        public int ProductId
+        {
+            get { return _productId; }
+            set
+            {
+                if (value != _productId)
+                {
+                    _productId = value;
+                    OnPropertyChanged("ProductId");
+                }
+            }
+        }
+
+        public string ProductName
+        {
+            get { return _productName; }
+            set
+            {
+                if (value != _productName)
+                {
+                    _productName = value;
+                    OnPropertyChanged("ProductName");
+                }
+            }
+        }
+
+        public decimal UnitPrice
+        {
+            get { return _unitPrice; }
+            set
+            {
+                if (value != _unitPrice)
+                {
+                    _unitPrice = value;
+                    OnPropertyChanged("UnitPrice");
+                }
+            }
+        }
+
+        #endregion // Properties
+    }
+}
---
+++ dot net playground/ViewModels/WebDownloaderViewModel.cs
@@ -0,0 +1,107 @@
+﻿using dot_net_playground.Models;
+using dot_net_playground.Models.Base;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace dot_net_playground.ViewModels
+{
+    public class WebDownloaderViewModel : ObservableObject
+    {
+        private ProductModel _currentProduct;
+        private ICommand _downloadProduct;
+        private ICommand _downloadProductAsync;
+        private long _fetchTime;
+
+        public ProductModel CurrentProduct
+        {
+            get { return _currentProduct; }
+            set
+            {
+                if (value != _currentProduct)
+                {
+                    _currentProduct = value;
+                    OnPropertyChanged("CurrentProduct");
+                }
+            }
+        }
+
+        public ICommand DownloadProductCommand
+        {
+            get
+            {
+                if (_downloadProduct == null)
+                {
+                    _downloadProduct = new RelayCommand(
+                        param => DownloadProduct()
+                    );
+                }
+                return _downloadProduct;
+            }
+        }
+
+        public ICommand DownloadProductAsyncCommand
+        {
+            get
+            {
+                if (_downloadProductAsync == null)
+                {
+                    _downloadProductAsync = new RelayCommand(
+                        param => DownloadProductAsync()
+                    );
+                }
+                return _downloadProductAsync;
+            }
+        }
+
+        public long FetchTime
+        {
+            get { return _fetchTime; }
+            set
+            {
+                if (value != _fetchTime)
+                {
+                    _fetchTime = value;
+                    OnPropertyChanged("FetchTime");
+                }
+            }
+        }
+
+        private void DownloadProduct()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            CurrentProduct = _DownloadProduct();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            FetchTime = elapsedMs;
+        }
+
+        private async void DownloadProductAsync()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            CurrentProduct = await Task.Run(() => _DownloadProduct());
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            FetchTime = elapsedMs;
+        }
+
+        private ProductModel _DownloadProduct()
+        {
+            Thread.Sleep(2000);
+
+            return new ProductModel
+            {
+                ProductId = new Random().Next(),
+                ProductName = string.Format("New Product {0}", new Random().Next()),
+                UnitPrice = new Random().Next()
+            };
+        }
+    }
+}
