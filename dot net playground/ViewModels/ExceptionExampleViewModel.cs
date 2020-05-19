189fea93cacb46755c812c8c149e61319202a2a8
rudinesurya <rudinesurya@gmail.com>
2020-05-19 20:21:35 +0100

    exceptions example

--- dot net playground/ViewModels/ExceptionExampleViewModel.cs
+++ dot net playground/ViewModels/ExceptionExampleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using dot_net_playground.Models.Base;
+﻿using dot_net_playground.Models;
+using dot_net_playground.Models.Base;
 using System;
 using System.Windows;
 using System.Windows.Input;
@@ -8,6 +9,7 @@ namespace dot_net_playground.ViewModels
     public class ExceptionExampleViewModel : ObservableObject
     {
         private ICommand _getArrayOutOfBoundsExceptionCommand;
+        private ICommand _getNullPointerExceptionCommand;
 
         public ICommand GetArrayOutOfBoundsExceptionCommand
         {
@@ -23,12 +25,40 @@ namespace dot_net_playground.ViewModels
             }
         }
 
+        public ICommand GetNullPointerExceptionCommand
+        {
+            get
+            {
+                if (_getNullPointerExceptionCommand == null)
+                {
+                    _getNullPointerExceptionCommand = new RelayCommand(
+                        param => GetNullPointerException()
+                    );
+                }
+                return _getNullPointerExceptionCommand;
+            }
+        }
+
         private void GetArrayOutOfBoundsException()
         {
             try
             {
                 var arr = new int[] { 1, 2, 3 };
                 var item = arr[5];
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Exception Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GetNullPointerException()
+        {
+            try
+            {
+                var p = new ProductModel();
+                p = null;
+                var id = p.ProductId;
             }
             catch (Exception e)
             {
