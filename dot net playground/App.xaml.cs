052adb242ec1a8ddd76bf503b6652db6b0b6b62d
rudinesurya <rudinesurya@gmail.com>
2020-05-27 12:28:29 +0100

    single instance application

--- dot net playground/App.xaml.cs
+++ dot net playground/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,24 @@ namespace dot_net_playground
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex _mutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "Dot Net Playground";
+            bool createdNew = false;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                Application.Current.Shutdown();
+                MessageBox.Show(string.Format("{0} is already running", appName));
+                return;
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
